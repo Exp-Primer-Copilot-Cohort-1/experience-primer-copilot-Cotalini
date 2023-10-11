1a4e631543a526e5f4988bfd1f513babd8c4727b
Cotali <76927007+Cotalini@users.noreply.github.com>
2023-10-11 22:00:06 +0000

    member js

--- member.js
+++ member.js
@@ -1,6 +1,12 @@
 function skillsMember() {
   return {
     restrict: 'E',
-    templateUrl: 'views/member/skills.html'
+    templateUrl: 'modules/skills/views/member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'vm',
+    bindToController: true,
+    scope: {
+      member: '='
+    }
   };
 }
